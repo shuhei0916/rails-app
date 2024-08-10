d2139aadb4d848be98b7ee24e313222bb57fdc35
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-10 23:38:10 +0900

    Add boostrap template

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,13 +1,13 @@
 class HelloController < ApplicationController
     def index
-        msg = '
-        <html>
-        <body>
-            <h1>Sample page</h1>
-            <p>this is rails sample</p>
-        </body>
-        </html>
-        '   
-        render html: msg.html_safe
+        # msg = '
+        # <html>
+        # <body>
+        #     <h1>Sample page</h1>
+        #     <p>this is rails sample</p>
+        # </body>
+        # </html>
+        # '   
+        # render html: msg.html_safe
     end
 end

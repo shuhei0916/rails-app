ebc97ca3029a8ff78e7b621b84ea71b9d7947695
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-15 20:16:11 +0900

    Add new files for mgsborad

--- app/controllers/msgboard_controller.rb
+++ app/controllers/msgboard_controller.rb
@@ -1,4 +1,26 @@
 class MsgboardController < ApplicationController
+  layout 'msgboard'
+  
+  def initialize
+    super 
+    begin
+      @msg_data = JSON.parse(File.read("data.txt"))
+    rescue
+      @msg_data = Hash.new
+    end
+    @msg_data.each do |key,obj|
+      if Time.now.to_i - key.to_i > 24*60*60 then
+        @msg_data.delete(key)
+      end
+    end
+    File.write("data.txt", @msg_data.to_json)
+  end
+
   def index
   end
+
+end
+
+class MyData
+  
 end
